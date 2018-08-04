bd7e7bec2c7c041da15e97fec3045b8da13921d4
Baconm145 <39271726+Baconm145@users.noreply.github.com>
2018-08-04 13:39:04 +0300

    hotfix
    removed jquery from backend

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const path = require('path')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-var $ = require('jQuery')
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     extname: '.hbs',
