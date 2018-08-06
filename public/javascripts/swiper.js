1a644d195aee22d2d4a2c2f7ccc8c6fcb36c51be
Baconm145 <39271726+Baconm145@users.noreply.github.com>
2018-08-06 16:12:42 +0300

    swiper hotfix
    swiper multiple call jam fixed
    now works correctly

--- public/javascripts/swiper.js
+++ public/javascripts/swiper.js
@@ -1,34 +1,38 @@
 jQuery( function($) {
-    $("#results_swiper_right").click( function() {
-        $( "#results_box" )
-        .queue(function() {
-            $( this )
-                .fadeOut ( { queue: false } )
-                .animate( { left: '+=500'}, 400 )
-                .dequeue();
-        })
-        .animate( {left: '-=1000'}, 0 )
-        .queue(function () {           
-            $(this)
-                .fadeIn ( { queue: false }, 1000 )
-                .animate( { left: '+=500'}, 400 )
-                .dequeue();
-        })
+    $("#results_swiper_right").click( function() {   
+        if ( $("#results_box").queue("fx").length == 0 ) {
+            $( "#results_box" )
+            .queue(function() {
+                $( this )
+                    .fadeOut ( { queue: false } )
+                    .animate( { left: '+=500'}, 400 )
+                    .dequeue();
+            })
+            .animate( {left: '-=1000'}, 0 )
+            .queue(function () {           
+                $(this)
+                    .fadeIn ( { queue: false }, 1000 )
+                    .animate( { left: '+=500'}, 400 )
+                    .dequeue();
+            });
+        };
     });
     $("#results_swiper_left").click( function() {
-        $( "#results_box" )
-        .queue(function() {
-            $( this )
-                .fadeOut ( { queue: false } )
-                .animate( { left: '-=500'}, 400 )
-                .dequeue();
-        })
-        .animate( {left: '+=1000'}, 0 )
-        .queue(function () {           
-            $(this)
-                .fadeIn ( { queue: false }, 1000 )
-                .animate( { left: '-=500'}, 400 )
-                .dequeue();
-        })
+        if ( $("#results_box").queue("fx").length == 0 ) {
+            $( "#results_box" )
+            .queue(function() {
+                $( this )
+                    .fadeOut ( { queue: false } )
+                    .animate( { left: '-=500'}, 400 )
+                    .dequeue();
+            })
+            .animate( {left: '+=1000'}, 0 )
+            .queue(function () {           
+                $(this)
+                    .fadeIn ( { queue: false }, 1000 )
+                    .animate( { left: '-=500'}, 400 )
+                    .dequeue();
+            });
+        };
     });
 });
