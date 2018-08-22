20837ab7da89b4ef4173b9558290bd3228c7dece
Baconm145 <39271726+Baconm145@users.noreply.github.com>
2018-08-22 16:06:05 +0300

    added ssl sert and https listen

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const app = require('./app')
 const port = 80
+var fs = require('fs')
+var http = require('http')
+var https = require('https')
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8')
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8')
 
-app.listen(port, function (err) {
+var credentials = {key: privateKey, cert: certificate};
+
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, function (err) {
   if (err) {
     throw err
   }
 
-  console.log(`server is listening on ${port}...`)
+  console.log(`server is listening on 80...`)
+});
+httpsServer.listen(443, function (err) {
+  if (err) {
+    throw err
+  }
+
+  console.log(`server is listening on 443...`)
 })
