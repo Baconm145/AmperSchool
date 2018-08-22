585f23d3e1ac09c641fbf83045c9a97685b55b24
Baconm145 <39271726+Baconm145@users.noreply.github.com>
2018-08-22 16:25:20 +0300

    added auto-redirect to https

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const port = 80
 var fs = require('fs')
 var http = require('http')
 var https = require('https')
+var httpsRedirect = require('express-https-redirect');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8')
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8')
 
 var credentials = {key: privateKey, cert: certificate};
 
-// your express configuration here
+app.use('*', httpsRedirect(true));
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
